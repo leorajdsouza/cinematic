5f827accdea9e89450875eb9fa95f1ac9c30de28
l30 <bux420@gmail.com>
2017-01-23 16:55:11 +0530

    Add files via upload

--- controller/app.js
+++ controller/app.js
@@ -9,6 +9,18 @@ var appConfig = {
 */
 
 var app = angular.module('tvApp', ['ngRoute']);
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
 app.controller('tvAppCtrl', function ($scope, showListService, $rootScope, $location) {
     $rootScope.isLoading = true;
     $scope.page_no = 1;
