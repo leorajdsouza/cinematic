4ba53863bf6f7cc562920d52f1ac8ede80510541
IMl30 <bux420@gmail.com>
2017-02-09 21:33:06 +0530

    Add files via upload

--- controller/app.js
+++ controller/app.js
@@ -111,9 +111,13 @@ app.controller('showsDetailsCtrl', function ($scope, showListService, $routePara
 */
 app.controller('showSearchCtrl', function ($scope, showListService, $rootScope, $location) {
     //check if ur in search page else dont navigate
+    // clear search query
     $scope.searchShow = function () {
-        $location.path("search");
-        showListService.searchShow($scope.searchCallback, $scope.search);
+        if ($scope.search != "" && $scope.search != undefined) {
+            $location.path("search");
+            showListService.searchShow($scope.searchCallback, $scope.search);
+        }
+
     }
 
     $scope.searchCallback = function (data) {
