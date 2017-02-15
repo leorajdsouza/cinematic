a2c08d9c0f33e93b2ad0f488c5bc40d85a8a9b1b
IMl30 <bux420@gmail.com>
2017-02-15 20:46:30 +0530

    Add files via upload

--- controller/app.js
+++ controller/app.js
@@ -6,66 +6,9 @@ To do
 - dont not remove any comment as comment contains features information
 - impliment state routing
 */
-var wachlistOffline = [{
-    "trakt": 1409,
-    "slug": "the-big-bang-theory",
-    "tvdb": 80379,
-    "imdb": "tt0898266",
-    "tmdb": 1418,
-    "tvrage": 8511
-}, {
-    "trakt": 1403,
-    "slug": "arrow",
-    "tvdb": 257655,
-    "imdb": "tt2193021",
-    "tmdb": 1412,
-    "tvrage": 30715
-}, {
-    "trakt": 1394,
-    "slug": "marvel-s-agents-of-s-h-i-e-l-d",
-    "tvdb": 263365,
-    "imdb": "tt2364582",
-    "tmdb": 1403,
-    "tvrage": 32656
-}, {
-    "trakt": 1393,
-    "slug": "the-walking-dead",
-    "tvdb": 153021,
-    "imdb": "tt1520211",
-    "tmdb": 1402,
-    "tvrage": 25056
-}, {
-    "trakt": 60300,
-    "slug": "the-flash-2014",
-    "tvdb": 279121,
-    "imdb": "tt3107288",
-    "tmdb": 60735,
-    "tvrage": 36939
-}, {
-    "trakt": 1390,
-    "slug": "game-of-thrones",
-    "tvdb": 121361,
-    "imdb": "tt0944947",
-    "tmdb": 1399,
-    "tvrage": 24493
-}, {
-    "trakt": 1611,
-    "slug": "supernatural",
-    "tvdb": 78901,
-    "imdb": "tt0460681",
-    "tmdb": 1622,
-    "tvrage": 5410
-}, {
-    "trakt": 19792,
-    "slug": "sherlock",
-    "tvdb": 176941,
-    "imdb": "tt1475582",
-    "tmdb": 19885,
-    "tvrage": 23433
-}]
 
 
-var app = angular.module('cinematicApp', ['ngRoute']);
+var app = angular.module('cinematicApp', ['ngRoute','ui.rCalendar']);
 var db;
 // app.constant('DB_CONFIG', {
 //     shows: {
@@ -79,26 +22,27 @@ app.run(function (TraktTVv2, $rootScope) {
     //https://api.trakt.tv/calendars/all/shows/2017-02-12/7?languages=en&status=returning%20series&countries=us
     //offline data
 
+    /* Get this weeks dates*/
+    var curr = new Date;
+    var firstday = new Date(curr.setDate(curr.getDate() - curr.getDay()));
 
-    var watchlist = [];
-
-    function WatchedCallback(data) {
-        angular.forEach(data, function (value, key) {
-            watchlist.push(value.show.ids);
-        }, watchlist);
-        $rootScope.watchlist = watchlist;
-        TraktTVv2.getShowCalendar($rootScope.watchlist,calendarCallback);
+    var date = new Date(firstday);
+    var day = date.getDay();
+    $rootScope.thisweek = []
+    for (var i = 0; i < 7; i++) {
+        if (i - day != 0) {
+            var days = i - day;
+            var newDate = new Date(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            $rootScope.thisweek.push(newDate);
+        } else
+            $rootScope.thisweek.push(date);
     }
 
-    TraktTVv2.getUserWatchlist("bux420", WatchedCallback);
-    //  $rootScope.watchlist = wachlistOffline;
 
-    function calendarCallback(data) {
-        $rootScope.Showcalendar = data; 
-    }
 
 
 });
+
 
 // }).run(function ($SQLite, DB_CONFIG) {
 //     $SQLite.init(function (init) {
--- controller/calendar.js
+++ controller/calendar.js
@@ -1,34 +1,161 @@
-app.controller('calendarCtrl', function ($scope, $rootScope) {
+app.controller('calendarCtrl', function ($scope, $rootScope, TraktTVv2) {
 
-    // console.log($rootScope.Showcalendar);
-    $scope.weeks = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-    var curr = new Date;
-    var firstday = new Date(curr.setDate(curr.getDate() - curr.getDay()));
-
-    $scope.today = new Date();
-
-    var date = new Date(firstday);
-    var day = date.getDay();
-
-    $scope.thisweek = []
-    for (var i = 0; i < 7; i++) {
-        if (i - day != 0) {
-            var days = i - day;
-            var newDate = new Date(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            $scope.thisweek.push(newDate);
-        } else
-            $scope.thisweek.push(date);
+    var watchlist = [];
+    function WatchedCallback(data) {
+        angular.forEach(data, function (value, key) {
+            watchlist.push(value.show.ids);
+        }, watchlist);
+        $rootScope.watchlist = watchlist;
+        TraktTVv2.getShowCalendar($rootScope.watchlist, calendarCallback);
     }
 
-    $scope.calendars = $rootScope.Showcalendar;
-    $scope.getdate = function (date) {
-        var newdate = new Date(date);
-        console.log(date);
-        return newdate.getDate();
+    TraktTVv2.getUserWatchlist("bux420", WatchedCallback);
+    //  $rootScope.watchlist = wachlistOffline;
+
+    function calendarCallback(data) {
+        $rootScope.Showcalendar = data;
+
+        $scope.today = new Date();
+        $scope.calendars = $rootScope.Showcalendar;
+        $scope.thisweek = $rootScope.thisweek;
+
+        console.log(JSON.stringify($scope.thisweek));
+        $scope.getdate = function (date) {
+            var newdate = new Date(date);
+            return newdate.getDate();
+        }
+
+        var ncal = [];
+        for (var i = 0; i < $scope.thisweek.length; i++) {
+            var newDate = new Date($scope.thisweek[i]);
+            var count = 0;
+            var prev = 0;
+            var tempArray = [];
+
+            for (var j = 0; j < $scope.calendars.length; j++) {
+                var caldate = new Date($scope.calendars[j].first_aired);
+                if (newDate.getDate() == caldate.getDate()) {
+                    if (prev == caldate.getDate()) {
+                        $scope.calendars[j - 1].show = true;
+                        tempArray.push($scope.calendars[j - 1]);
+                        $scope.calendars[j].show = true;
+                        tempArray.push($scope.calendars[j]);
+                        count++
+                    } else {
+                        count++
+                    }
+                    prev = newDate.getDate();
+                }
+            }
+            if (count > 1) {
+                ncal.push(tempArray);
+            } else if (count == 1) {
+                $scope.calendars[i].show = true;
+                ncal.push($scope.calendars[i]);
+            } else if (count === 0) {
+                ncal.push({ "first_aired": newDate, "show": false });
+            }
+
+        }
+        $scope.calendars = ncal;
+        console.log(ncal);
     }
- 
-    $scope.shows = $rootScope.Showcalendar;
+
+    //    $scope.weeks = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 
+    // $scope.mode = "week";
+    // $scope.showEventDetail = false;
+
+
+    // var showCalendar = [];
+    // for (var i = 0; i < $scope.thisweek.length; i++) {
+    //     var mainDate = new Date($scope.thisweek[i]);
+    //     var tcount = varCount($scope.calendars, mainDate.getDate());
+    //     if (tcount == 0) {
+    //         showCalendar.push({ "first_aired": mainDate, "show": false });
+    //     }
+    //     if (tcount == 1) {
+    //         showCalendar.push($scope.calendars[i]);
+    //     }
+    //     if (tcount > 1) {
+    //         var temparrat = [];
+    //         for (i = 0; i < tcount; i++) {
+    //            // showCalendar.push($scope.calendars[i]);
+
+    //         }
+    //     }
+    // }
+
+    //console.log(JSON.stringify($scope.calendars[0]));
+
+    //    var temp = { "first_aired": "2017-02-13T02:00:00.000Z", "show": false }
+
+    // var showCalendar = [];
+    // for (var i = 0; i < $scope.thisweek.length; i++) {
+    //     var mainDate = new Date($scope.thisweek[i]);
+    //     var tcount = varCount($scope.calendars, mainDate.getDate());
+    //     if (tcount == 0) {
+    //         showCalendar.push({ "first_aired": mainDate, "show": false });
+    //     }
+    //     if (tcount == 1) {
+    //         showCalendar.push($scope.calendars[i]);
+    //     }
+    //     if (tcount > 1) {
+    //         var temparrat = [];
+    //         for (i = 0; i < tcount; i++) {
+    //            // showCalendar.push($scope.calendars[i]);
+
+    //         }
+    //     }
+    // }
+    //console.log(showCalendar);
+
+    // function varCount(array, val) {
+    //     var count = 0;
+    //     for (var i = 0; i < array.length; i++) {
+    //         if (array[i] == val) {
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
+
+    // function createRandomEvents() {
+    //     var events = [];
+    //     for (var i = 0; i < 50; i += 1) {
+    //         var date = new Date();
+    //         var eventType = Math.floor(Math.random() * 2);
+    //         var startDay = Math.floor(Math.random() * 90) - 45;
+    //         var endDay = Math.floor(Math.random() * 2) + startDay;
+    //         var startTime;
+    //         var endTime;
+    //         if (eventType === 0) {
+    //             startTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + startDay));
+    //             if (endDay === startDay) {
+    //                 endDay += 1;
+    //             }
+    //             endTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + endDay));
+    //             events.push({
+    //                 title: 'All Day - ' + i,
+    //                 startTime: startTime,
+    //                 endTime: endTime,
+    //                 allDay: true
+    //             });
+    //         } else {
+    //             var startMinute = Math.floor(Math.random() * 24 * 60);
+    //             var endMinute = Math.floor(Math.random() * 180) + startMinute;
+    //             startTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + startDay, 0, date.getMinutes() + startMinute);
+    //             endTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + endDay, 0, date.getMinutes() + endMinute);
+    //             events.push({
+    //                 title: 'Event - ' + i,
+    //                 startTime: startTime,
+    //                 endTime: endTime,
+    //                 allDay: false
+    //             });
+    //         }
+    //     }
+    //     console.log(events);
+    //     return events;
+    // }
 });
